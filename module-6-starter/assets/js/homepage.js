46283702f3f096993a831544fd10e32b3cddcc02
Daniel O <danielolvera2188@gmail.com>
2021-06-15 14:00:48 -0600

    added language buttons

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -2,16 +2,8 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
-//var getUserRepos = function () {
-//fetch("https://api.github.com/users/octocat/repos").then(function (response) {
-//response.json().then(function (data) {
-//console.log(data);
-//});
-//});
-//console.log("outside");
-//};
-//getUserRepos();
 var getUserRepos = function (user) {
   //format the github api url
   var apiUrl = "http://api.github.com/users/" + user + "/repos";
@@ -31,6 +23,23 @@ var getUserRepos = function (user) {
       //notice this '.catch' getting chained onto the end of the then method
       alert("Unable to connect to GitHub");
     });
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found!");
+    }
+  });
 };
 
 //executes when a user submits info to the form
@@ -100,4 +109,15 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
